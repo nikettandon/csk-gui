4d87e7151deeb21c8c958ffdfa5fae19e9118058
chariman <charlesdarwis03@gmail.com>
2014-07-01 16:10:33 +0200

    Modified codes to separate results based on different meronym type (member meronym, part meronym, substance meronym).

--- genericbrowser/src/model/PartOfSeedsGenerator.java
+++ genericbrowser/src/model/PartOfSeedsGenerator.java
@@ -28,9 +28,11 @@ import util.FileUtil;
 public class PartOfSeedsGenerator {
 	
 	public static class PartOfSeeds {
+		public String description;
 		public List<PartOfSeed> seeds;
 		
-		public PartOfSeeds(){
+		public PartOfSeeds(String desc){
+			this.description = desc;
 			this.seeds = new ArrayList<>();
 		}
 		
@@ -59,51 +61,53 @@ public class PartOfSeedsGenerator {
 	}
 	
 	private static Database db;
-	private static PartOfSeeds seeds = new PartOfSeeds();
-    private static int count=0;
-
+	private static PartOfSeeds seeds ;
+    private static int count;
+    private static  Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static String json;
 	/**
 	 * SQL to join tables wn_mereonymy_m or wn_mereonymy_p or wn_mereonymy_s with wn_synsets
 	 * @param type : suffix of wn_mereonymy {m, p, s}
 	 * @return
 	 */
-	public static String buildSql(String type){
+	public static String buildSql(String meronymType){
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT p.from_ss AS fr_synset_id, f.word AS fr_word, ")
 			.append("f.sense_number AS fr_sense_number, ")
 			.append("p.to_ss AS to_synset_id, t.word AS to_word, t.sense_number AS to_sense_number ")
-			.append("FROM wordnet.wn_mereonymy_"+type+" p " )
+			.append("FROM wordnet.wn_mereonymy_"+meronymType+" p " )
 			.append("JOIN wordnet.wn_synsets f ON (p.from_ss = f.synset_id) ")
 			.append("JOIN wordnet.wn_synsets t ON (p.to_ss = t.synset_id) ")
 			.append("");
 		return sql.toString();
 	}
 	
-	public static void getSeeds(String sql) throws SQLException{
-		ResultSet rs = DBConnector.q(sql);
+	public static void printSeeds(String meronymType, String description) throws SQLException{
+		count = 0;
+		String outFile = "WebContent/data/partOfSeeds_"+meronymType+".json";
+		seeds = new PartOfSeeds(description);
+		ResultSet rs = DBConnector.q(buildSql(meronymType));
 	    rs.toString();
 	    while (rs.next()){
 	       seeds.addSeed(new PartOfSeed(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6)));
 	       count++;
 	    }
+	    json = gson.toJson(seeds);
+	    FileUtil.writeFileUserDefinedObject(outFile, json, false);
+	    System.out.println("Done generating "+count+" "+meronymType+" seeds.");
+		 
 	}
 	public static void main(String[] args) throws SQLException{
 	     
 		 
 	     if(db == null) db = DBConnector.getDB();
 		 
-	     getSeeds(buildSql("m"));
-	     getSeeds(buildSql("p"));
-	     getSeeds(buildSql("s"));
+	     printSeeds("m", "member meronym");
+	     printSeeds("p", "part meronym");
+	     printSeeds("s", "substance meronym");
 	     
 	     DBConnector.closeConnections();
-	     
-	     String outFile = "WebContent/data/partOfSeeds.json";
-	     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		 String json = gson.toJson(seeds);
-		 FileUtil.writeFileUserDefinedObject(outFile, json, false);
-		 System.out.println("Done generating "+count+" seeds.");
-		 //101801 seeds
+	     //101801 seeds
 	}
 	
 }
